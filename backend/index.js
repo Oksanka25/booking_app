db66b113f4ac428d9b8d109522a5b4743a4fd356
Oksanka25 <chekoksa25@gmail.com>
2022-11-12 15:58:25 -0500

    Create the first route

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+import authRoute from "./routes/auth.js"
+
+const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to MongoDB!");
+    } catch (error) {
+        throw error
+
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected!");
+});
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected!");
+});
+
+app.get("/", (req, res) => {
+    res.send("hello first request")
+})
+
+// Middleware
+
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+    connect()
+    console.log("Connected to the backend.");
+})
