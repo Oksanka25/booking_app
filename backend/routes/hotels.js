64b5afb431ddb18f4b0ee3cb186e8ed3c385de98
Oksanka25 <chekoksa25@gmail.com>
2022-11-12 18:49:19 -0500

    User verification work

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,22 +1,23 @@
 import express from "express";
 import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotelRooms, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // Create 
-router.post("/", createHotel)
+router.post("/", verifyAdmin, createHotel)
 // Update
-router.put("/:id", updateHotel)
+router.put("/:id", verifyAdmin, updateHotel)
 // Delete
 router.delete("/:id", deleteHotel)
 // Get A hotel
-router.get("/:id", getHotel)
+router.get("/:id", verifyAdmin, getHotel)
 // Get ALL hotels
-router.get("/", getHotels)
-router.get("/countByCity", countByCity)
-router.get("/countByType", countByType)
-router.get("/room/:id", getHotelRooms)
+router.get("/", verifyAdmin, getHotels)
+router.get("/countByCity", verifyAdmin, countByCity)
+router.get("/countByType", verifyAdmin, countByType)
+router.get("/room/:id", verifyAdmin, getHotelRooms)
 
 
 export default router
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,17 +1,19 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
+
 
 const router = express.Router();
 
 
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 //GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 //GET ALL
-router.put("/", getUsers);
+router.put("/", verifyAdmin, getUsers);
 
 
 
